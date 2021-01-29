a1ec920ab7b381439b1674784b0555d0a786f9ea
Muhammad Fauzan Fadhlulbarr <52595452+muhammadfauzanfadhlulbarr@users.noreply.github.com>
2021-01-29 14:38:23 +0700

    Update MainViewPelanggan.java

--- src/edu/muhammadfauzan/latihanmvcdjbc/view/MainViewPelanggan.java
+++ src/edu/muhammadfauzan/latihanmvcdjbc/view/MainViewPelanggan.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Nama : Muhammad Fauzan Fadhlulbarr
+ * Nim : 10119176
+ * Kelas : IF10K
  */
 package edu.muhammadfauzan.latihanmvcdjbc.view;
 
